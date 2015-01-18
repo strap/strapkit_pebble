668eb58f707a8fce3d12b6e632239439c5df7f1f
Marcelle Bonterre <marcelle.bonterre@gmail.com>
2015-01-17 20:06:38 -0500

    add support for all pebble.js text view properties

--- bin/templates/project/__PROJECT_NAME__SM/src/js/strapkit_modules/text.js
+++ bin/templates/project/__PROJECT_NAME__SM/src/js/strapkit_modules/text.js
@@ -4,16 +4,26 @@ var View = require('./view.js');
 
 var TextView = function(config){
 	View.call(this,config);
+	
 	this._config = config || {};
 	this._align = config.align || 'center';
 	this._position = config.position || new Vector2(0, 50);
 	this._size = config.size || new Vector2(144, 30);
 	this._text = config.text || '';
+	this._color = config.color || 'white';
+	this._overflow = config.overflow || 'ellipsis';
+	this_.borderColor = config.borderColor || 'clear';
+	this._backgroundColor = config.backgroundColor || 'clear';
+
 	this.pebbleView = new PebbleUI.Text({
     	position: this._position,
     	size: this._size,
     	font: 'gothic-24-bold',
     	text: this._text,
+    	color: this._color,
+    	textOverflow: this._overflow,
+    	borderColor: this_.borderColor,
+    	backgroundColor: this._backgroundColor,
     	textAlign: this._align
   	});
 };
